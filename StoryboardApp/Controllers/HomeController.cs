0976a36abfaf57d9a900387c50171bc00275deb1
nodirakosimova <nkosimova06@gmail.com>
2022-01-05 02:26:20 +0500

    init project

---
+++ StoryboardApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace StoryboardApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EasyStoryboarding()
+        {
+            return View();
+        }
+        
+        public IActionResult MediumStoryboarding()
+        {
+            return View();
+        }
+        
+        public IActionResult HardStoryboarding()
+        {
+            return View();
+        }
+        
+    }
+}
